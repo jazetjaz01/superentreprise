79cf09cf7d1323e34f24811a41db073fd862fc53
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-08-09 14:54:00 +0200

    update navbar

---
+++ components/hero.tsx
@@ -0,0 +1,38 @@
+import { ArrowUpRight, CirclePlay } from "lucide-react";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+export default function Hero() {
+  return (
+    <div className="flex min-h-screen items-center justify-center px-6 py-12 border-8">
+      <div className="mx-auto grid w-full max-w-(--breakpoint-xl) gap-16 lg:grid-cols-2">
+        <div>
+          <Badge className="rounded-full border-border py-1" variant="secondary" render={<Link href="#" />}>Just released v1.0.0 <ArrowUpRight className="ml-1 size-4" /></Badge>
+          <h1 className="mt-6 max-w-[17ch] font-medium text-4xl leading-[1.2]! tracking-[-0.04em] md:text-5xl lg:text-[2.75rem] xl:text-[3.25rem]">
+            Your complete
+            <br /> UI building toolkit
+          </h1>
+          <p className="mt-4 max-w-[60ch] text-foreground/60 text-lg sm:mt-6 sm:text-xl/normal">
+            Explore a collection of Shadcn UI blocks and components, ready to
+            preview and copy. Streamline your development workflow with
+            easy-to-implement examples.
+          </p>
+          <div className="mt-8 flex items-center gap-4 sm:mt-12">
+            <Button className="rounded-full" size="lg">
+              Get Started <ArrowUpRight className="h-5! w-5!" />
+            </Button>
+            <Button
+              className="rounded-full shadow-none"
+              size="lg"
+              variant="outline"
+            >
+              <CirclePlay className="h-5! w-5!" /> Watch Demo
+            </Button>
+          </div>
+        </div>
+        <div className="mt-auto aspect-video w-full rounded-xl bg-accent" />
+      </div>
+    </div>
+  );
+}
