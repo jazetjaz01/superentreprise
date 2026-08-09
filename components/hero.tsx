f8ce47f6d4d9daaae5b66424e6878cdd6843bc0b
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-08-09 15:25:16 +0200

    update navbar

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,8 +5,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <div className="flex min-h-screen items-center justify-center px-6 py-12 border-8">
-      <div className="mx-auto grid w-full max-w-(--breakpoint-xl) gap-16 lg:grid-cols-2">
+    <div className="flex items-center justify-center px-6 pt-12 pb-20 ">
+      <div className="mx-auto grid w-full max-w-(--breakpoint-2xl) gap-16 lg:grid-cols-2">
         <div>
           <Badge className="rounded-full border-border py-1" variant="secondary" render={<Link href="#" />}>Just released v1.0.0 <ArrowUpRight className="ml-1 size-4" /></Badge>
           <h1 className="mt-6 max-w-[17ch] font-medium text-4xl leading-[1.2]! tracking-[-0.04em] md:text-5xl lg:text-[2.75rem] xl:text-[3.25rem]">
