df85450035716718af1085fb0e21d4860078954e
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-20 09:59:07 +0200

    languages versions

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+import { Link } from "@/i18n/navigation";
 
 export const Logo = () => (
-  <Image src="/logose.svg" alt="Logo" width={32} height={32} priority />
+  <Link href="/">
+    <Image src="/logose.svg" alt="Logo" width={32} height={32} priority />
+  </Link>
 );
