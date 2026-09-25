cde48ae3dd7dec4afa54bf80c7d082012a6268d8
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-25 23:47:51 +0200

    update article

--- app/[locale]/articles/[slug]/page.tsx
+++ app/[locale]/articles/[slug]/page.tsx
@@ -92,7 +92,7 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
         <ArticleBanner />
 
         <CardContent className="p-4 sm:p-6">
-          <div className=" bg-slate-100 p-6 sm:p-8">
+          <div className="bg-slate-100 p-8 sm:p-12">
             <p className="text-sm text-muted-foreground">
               {t("publishedTimeAgo", { time: format.relativeTime(new Date(article.created_at)) })}
             </p>
