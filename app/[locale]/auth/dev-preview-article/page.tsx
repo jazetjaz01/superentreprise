5be6b18b06d956bdccb5e7562b764e642dcdc9b9
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-25 13:32:02 +0200

    update article

--- app/[locale]/auth/dev-preview-article/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ArticleForm } from "@/components/article-form";
-
-export default function DevPreviewArticlePage() {
-  return (
-    <ArticleForm userId="preview-user" authorName="Alami Er'haidi" authorAvatarUrl={null} />
-  );
-}
