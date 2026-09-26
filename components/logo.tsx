54a1f560b8f2bae2d2620090e3890060946be606
Alami ERHAIDI <alamierhaidi@iMac-de-Alami-2.local>
2026-09-26 21:01:04 +0200

    update followers

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 import { Link } from "@/i18n/navigation";
 
 export const Logo = () => (
-  <Link href="/">
+  <Link href="/" className="flex items-center gap-2">
     <Image src="/logose.svg" alt="Logo" width={32} height={32} priority />
+    <p className="font-semibold">superentreprise</p>
   </Link>
 );
